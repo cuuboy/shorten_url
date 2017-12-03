5564544404f8ace30e698ce6afff814e693803ac
cuuboy <yuzhe.c85@gmail.com>
2017-12-03 12:22:18 +0800

    Correct class name

--- db/migrate/20171203021310_change_referral_to_referer.rb
+++ db/migrate/20171203021310_change_referral_to_referer.rb
@@ -1,4 +1,4 @@
-class ChangeReferralToReferrer < ActiveRecord::Migration[5.1]
+class ChangeReferralToReferer < ActiveRecord::Migration[5.1]
   def change
     rename_column :visits, :referral, :referer
   end
